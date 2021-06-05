8fa5e2cc13e9f4afd4cd196cbfc3086dda0037b1
Arnav Sharma <68996768+Avipahadi@users.noreply.github.com>
2021-06-05 15:42:35 +0530

    Update main.js

--- main.js
+++ main.js
@@ -1,29 +1,39 @@
 var click = 1;
 
-function s_h() {
-    if (click == 1) {
-        document.getElementById("hint").style.display = "block";
-        document.getElementById("click").value = "Hide object names";
-        click -= 1;
-    } else {
-        document.getElementById("hint").style.display = "none";
-        document.getElementById("click").value = "Show object names";
-        click += 1;
-    }
+function setup() {
+  canvas = createCanvas(480, 380);
+  canvas.center();
+  video = createCapture(VIDEO);
+  video.hide();
+}
+
+function draw() {
+  image(video, 0, 0, 480, 380);
+}
+
+function start() {
+  objectDetector = ml5.objectDetector('cocossd', modelLoaded);
+  document.getElementById("status").innerHTML = "Status: Detecting Objects";
+  object_name = document.getElementById("nameofobject").value;
+}
+
+function modelLoaded() {
+  console.log("Model Loaded!");
+  status = true;
 }
 
 function myFunction(event) {
     var x = event.which || event.keyCode;
     if (x == 47) {
         document.getElementById("nameofobject").focus();
+        document.getElementById("nameofobject").innerHTML = "";
         document.getElementById("nameofobject").value = "";
+
     }
 }
 
-document.addEventListener('contextmenu', event => event.preventDefault());
-
 function myalert() {
-    document.getElementById("alert").style.display = "block";
+    document.getElementById("alert").style.display = "none";
     setTimeout(function () {
         document.getElementById("alert").style.display = "none";
     }, 5000);
